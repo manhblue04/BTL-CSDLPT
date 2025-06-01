7f27433ebffb51a9cd170c857327b9f04824e4b2
Manh Blue <manhducnb204@gmail.com>
2025-06-01 11:00:29 +0700

    manh

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from src.load import LoadRatings
 from src.rangePartition import Range_Partition, Range_Insert
 
 # D:/Năm 3/Kỳ 2/Cơ sở dữ liệu phân tán/BTL-CSDLPT/tests/test_data.dat
-LoadRatings("D:/Download/CSDLPT/BTL-CSDLPT/tests/test_data.dat")  # Đường dẫn tuyệt đối
+LoadRatings("D:/Năm 3/Kỳ 2/Cơ sở dữ liệu phân tán/BTL-CSDLPT/tests/test_data.dat")  # Đường dẫn tuyệt đối
+
 print("Dữ liệu đã được nạp vào bảng Ratings.")
 
-Range_Partition("ratings", 3)
-Range_Insert("ratings", 2, 1000, 3.33)
+Range_Partition("ratings", 4)
+
+Range_Insert("ratings", 3, 5000, 2.5)
