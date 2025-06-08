a37782d50120220fa6d0c9ccc9144834ce3c2d2a
coderlord2004 <duyn38520@gmail.com>
2025-06-08 17:12:50 +0700

    optimize roundrobin function speed

--- src/Assignment1Tester.py
+++ src/Assignment1Tester.py
@@ -10,8 +10,8 @@ RROBIN_TABLE_PREFIX = 'rrobin_part'
 USER_ID_COLNAME = 'userid'
 MOVIE_ID_COLNAME = 'movieid'
 RATING_COLNAME = 'rating'
-INPUT_FILE_PATH = 'test_data.dat'
-ACTUAL_ROWS_IN_INPUT_FILE = 20  # Number of lines in the input file
+INPUT_FILE_PATH = 'ratings.dat'
+ACTUAL_ROWS_IN_INPUT_FILE = 10000054  # Number of lines in the input file
 
 import psycopg2
 import traceback
@@ -28,28 +28,28 @@ if __name__ == '__main__':
 
             # testHelper.deleteAllPublicTables(conn)
 
-            # [result, e] = testHelper.testloadratings(MyAssignment, RATINGS_TABLE, INPUT_FILE_PATH, conn, ACTUAL_ROWS_IN_INPUT_FILE)
-            # if result :
-            #     print("loadratings function pass!")
-            # else:
-            #     print("loadratings function fail!")
+            [result, e] = testHelper.testloadratings(MyAssignment, RATINGS_TABLE, INPUT_FILE_PATH, conn, ACTUAL_ROWS_IN_INPUT_FILE)
+            if result :
+                print("loadratings function pass!")
+            else:
+                print("loadratings function fail!")
                 
-            # [result, e] = testHelper.testrangepartition(MyAssignment, RATINGS_TABLE, 5, conn, 0, ACTUAL_ROWS_IN_INPUT_FILE)
-            # if result :
-            #     print("rangepartition function pass!")
-            # else:
-            #     print("rangepartition function fail!")
+            [result, e] = testHelper.testrangepartition(MyAssignment, RATINGS_TABLE, 5, conn, 0, ACTUAL_ROWS_IN_INPUT_FILE)
+            if result :
+                print("rangepartition function pass!")
+            else:
+                print("rangepartition function fail!")
 
-            # # ALERT:: Use only one at a time i.e. uncomment only one line at a time and run the script
-            # [result, e] = testHelper.testrangeinsert(MyAssignment, RATINGS_TABLE, 100, 2, 3, conn, '2')
-            # # [result, e] = testHelper.testrangeinsert(MyAssignment, RATINGS_TABLE, 100, 2, 0, conn, '0')
-            # if result:
-            #     print("rangeinsert function pass!")
-            # else:
-            #     print("rangeinsert function fail!")
+            # ALERT:: Use only one at a time i.e. uncomment only one line at a time and run the script
+            [result, e] = testHelper.testrangeinsert(MyAssignment, RATINGS_TABLE, 100, 2, 3, conn, '2')
+            # [result, e] = testHelper.testrangeinsert(MyAssignment, RATINGS_TABLE, 100, 2, 0, conn, '0')
+            if result:
+                print("rangeinsert function pass!")
+            else:
+                print("rangeinsert function fail!")
 
 
-            # testHelper.deleteAllPublicTables(conn)
+            testHelper.deleteAllPublicTables(conn)
             MyAssignment.loadratings(RATINGS_TABLE, INPUT_FILE_PATH, conn)
 
             [result, e] = testHelper.testroundrobinpartition(MyAssignment, RATINGS_TABLE, 5, conn, 0, ACTUAL_ROWS_IN_INPUT_FILE)
@@ -66,7 +66,6 @@ if __name__ == '__main__':
                 print("roundrobininsert function pass!")
             else:
                 print("roundrobininsert function fail!")
-
 
 
             # choice = input('Press enter to Delete all tables? ')
